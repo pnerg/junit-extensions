8777b8a2a742f2e28a2bed137679c35d8e8e32ca
Peter Nerg <peter.nerg@gmail.com>
2015-12-31 15:48:40 +0100

    # Implemented CollectionAssert.assertIsEmpty

---
+++ src/test/java/junitextensions/TestCollectionsAssert.java
@@ -0,0 +1,41 @@
+/**
+ *  Copyright 2015 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package junitextensions;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+/**
+ * Test the class {@link CollectionsAssert}.
+ * @author Peter Nerg
+ */
+public class TestCollectionsAssert extends BaseAssert implements CollectionsAssert {
+
+	@Test
+	public void assertIsEmpty_withEmpty() {
+		assertIsEmpty(Collections.emptyList());
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void assertIsEmpty_withNonEmpty() {
+		Set<Object> set = new HashSet<>();
+		set.add(new Object());
+		assertIsEmpty(set);
+	}
+}
