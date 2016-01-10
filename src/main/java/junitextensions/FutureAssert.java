4c7176bf8d795bfaf773af49aa492f067474c7f1
Peter Nerg <peter.nerg@gmail.com>
2016-01-10 08:08:30 +0100

    #10 implemented FutureAssert.assertSuccess

---
+++ src/main/java/junitextensions/FutureAssert.java
@@ -0,0 +1,48 @@
+/**
+ *  Copyright 2016 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package junitextensions;
+
+import static org.junit.Assert.assertTrue;
+
+import javascalautils.Option;
+import javascalautils.Success;
+import javascalautils.Try;
+import javascalautils.concurrent.Future;
+
+/**
+ * Provides additional assert operations related to the {@link Future} class.
+ * @author Peter Nerg
+ * @since 1.1
+ */
+public interface FutureAssert extends NotNullAssert, TryAssert {
+
+    /**
+     * Asserts that the provided {@link Future} is {@link Future#isCompleted() completed} as well as the result is a {@link Success}.
+     * @param <T> The type of the Future
+     * @param future The Future instance to Assert
+     * @since 1.1
+     */
+	default <T> void assertSuccess(Future<T> future) {
+    	assertObjectNotNull(future);
+		Option<Try<T>> option = future.value();
+		
+		//if we get None then the Future is not finished
+		assertTrue("The Future is not completed.", option.isDefined());
+		
+		//now we assert the result (Try) of the Future
+		assertSuccess(option.get());
+	}
+}
